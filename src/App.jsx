641d766a72f134a61b8ad86505c7a9549053bc12
WisleyAraujo <wisleyaraujosanstos@gmail.com>
2024-03-19 14:48:19 -0300

    colocando toda a logica dentro do component produto, as proximas aulas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
 import "./App.css";
 import React from "react";
-import Button from "./components/Button.jsx";
 import Produto from "./components/Produto.jsx";
 function App() {
-  const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
-
-  const fetchDados = async (event) => {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
-    );
-    const json = await response.json();
-    setDados(json);
-    setCarregando(false);
-  };
-
   return (
     <div>
-      <button style={{ margin: ".5rem" }} onClick={fetchDados}>
-        notebook
-      </button>
-      <button style={{ margin: ".5rem" }} onClick={fetchDados}>
-        smartphone
-      </button>
-      <button style={{ margin: ".5rem" }} onClick={fetchDados}>
-        tablet
-      </button>
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados ? <Produto dados={dados} /> : ""}
+      <Produto />
     </div>
   );
 }
--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 
-const Produto = ({ dados }) => {
+const Produto = () => {
+  const [dados, setDados] = React.useState(null);
+
+  const fetchDados = async (event) => {
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
+    );
+    const json = await response.json();
+    setDados(json);
+  };
   return (
     <div>
-      <h1>{dados.nome}</h1>
-      <p>R$ {dados.preco}</p>
-      <img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
+      <button style={{ margin: ".5rem" }} onClick={fetchDados}>
+        notebook
+      </button>
+      <button style={{ margin: ".5rem" }} onClick={fetchDados}>
+        smartphone
+      </button>
+      <button style={{ margin: ".5rem" }} onClick={fetchDados}>
+        tablet
+      </button>
+      {dados && (
+        <>
+          <h1>{dados.nome}</h1>
+          <p>R$ {dados.preco}</p>
+          <img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
+        </>
+      )}
     </div>
   );
 };
